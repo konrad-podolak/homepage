65b74eed89b84b4a2d3b4c9e47fcbc353be6fcdf
Konrad Podolak <kon.podolak@gmail.com>
2020-05-13 22:05:12 +0200

    frame borders added to .about__image

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 const heading = document.querySelector(".main-header__my-name--js");
-heading.innerHTML = 'Jolka Mariolka'
+heading.innerHTML = 'Jolka Mariolka';
+
+const nala = document.querySelector(".about-nala--js");
+nala.innerHTML = 'Nala i my cat and she is so cute';
