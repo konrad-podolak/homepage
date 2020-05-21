dc5c18dfbc9065fc5cedad9943765640a5062947
Konrad Podolak <kon.podolak@gmail.com>
2020-05-21 21:06:28 +0200

    new site useful to learning GRIT

--- main.js
+++ main.js
@@ -1,30 +1,69 @@
 const heading = document.querySelector(".main-header__my-name--js");
-heading.innerHTML = 'Konrad Podolak Frontend Developer';
+heading.innerHTML = "Konrad Podolak Frontend Developer";
 
-const nala = document.querySelector(".about-nala--js");
-nala.innerHTML = 'Nala i my cat and she is so cute';
 
 function calculate(myNumber) {
-    return myNumber*7;
+  return myNumber * 7;
 }
 
 const myResult = calculate(9);
 console.log(myResult);
 
 const greet = (firtsName, age) => {
-    console.log(`Hej Ty nazywam się ${firtsName} i mam ${age} lat :D.`);
-}
-greet('Konrad', 26);
+  console.log(`Hej Ty nazywam się ${firtsName} i mam ${age} lat :D.`);
+};
+greet("Konrad", 26);
 
 function createContent(querySelectorContent, content) {
-    const element = document.querySelector(querySelectorContent);
-    element.innerHTML = content;
+  const element = document.querySelector(querySelectorContent);
+  element.innerHTML = content;
 }
 
-createContent('.for-fun__java--header--js','Nagłówek został zmieniony w JS');
+createContent(".for-fun__java--header--js", "Nagłówek został zmieniony w JS");
 
 const newCreateContent = (piece, content) => {
-    const element = document.querySelector(piece);
-    element.innerHTML = content;
+  const element = document.querySelector(piece);
+  element.innerHTML = content;
+};
+newCreateContent(".for-fun__java--aside--js", "Autorem testu są zmiany w JS");
+
+/* tworzenie obiektów*/
+const deathStar = {
+  diameter: 120000,
+  fire: (target) => {
+    console.log(`${target} destroyed buuum`);
+  },
+  isOperating: true,
+  levels: 357,
+  name: "Death Star",
+  population: 10000,
+  comander: {
+    name: "Darth Vader",
+    age: 44,
+  },
+};
+
+console.log(deathStar);
+
+const daria = {
+  height: 169,
+  age: 24,
+  address: {
+    street: "Nasypowa",
+    city: "Gdynia",
+  },
+};
+
+if(daria.age <= 24)  {
+    console.log('Daria to młoda sztunia');
+}else  {
+    console.log('ale stara baba');
+};
+
+const messi = {
+    nationality: 'argentina',
+    height: 172,
+    age: 32,
 }
-newCreateContent('.for-fun__java--aside--js', 'Autorem testu są zmiany w JS');
+
+console.log(messi);
